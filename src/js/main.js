264008dec86082c0a8721532ce3457acc2d8f14b
Dima Mironenko <City7gor@gmail.com>
2022-12-12 13:23:33 +0200

    12.12.2022

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,29 @@
 import development from "./development.js";
 import { showPopup, closePopup } from "./popup.js";
 import hamburger from "./base/hamburger.js";
+// import barba from '@barba/core'
+// import gsap from 'gsap'
 
 development();
 hamburger();
+
+// barba.init({
+//   transitions: [{
+//     name: 'opacity-transition',
+//     leave(data) {
+//       return gsap.to(data.current.container, {
+//         opacity: 0,
+//         duration: 0.3
+//       });
+//     },
+//     enter(data) {
+//       return gsap.from(data.next.container, {
+//         opacity: 0,
+//         duration: 0.3
+//       });
+//     }
+//   }]
+// })
 
 // Header Scrolled
 window.addEventListener("scroll", () => {
@@ -18,12 +38,14 @@ window.addEventListener("scroll", () => {
 document.querySelectorAll("form").forEach((item) => {
   item.addEventListener("submit", (e) => {
     e.preventDefault();
+    const formData = new FormData(item)
+    console.log(formData);
     showPopup(".popup");
   });
 
-  document.querySelector(".popup").addEventListener("scroll", (e) => {
-    console.log(e);
-  });
+  // document.querySelector(".popup").addEventListener("scroll", (e) => {
+  //   console.log(e);
+  // });
 });
 
 window.addEventListener("click", (e) => {
