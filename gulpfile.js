664f89308bdd85f260959d64361f9c4276194b96
City7g <City7gor@gmail.com>
2023-01-03 21:33:06 +0200

    Format gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,43 @@
-import gulp from "gulp";
-import fs from "fs";
-import gulpSass from "gulp-sass";
-import nodeSass from "sass";
-import pug from "gulp-pug";
-import webpack from "webpack-stream";
-import browsersync from "browser-sync";
+import gulp from 'gulp'
+import fs from 'fs'
+import gulpSass from 'gulp-sass'
+import nodeSass from 'sass'
+import pug from 'gulp-pug'
+import webpack from 'webpack-stream'
+import browsersync from 'browser-sync'
 
-const sass = gulpSass(nodeSass);
-browsersync.create();
+const sass = gulpSass(nodeSass)
+browsersync.create()
 
 const browserSync = () => {
   browsersync.init({
     server: {
-      baseDir: "./dist/",
+      baseDir: './dist/',
     },
     // host: "127.0.0.1",
     // online: true,
     // port: 8080,
     notify: false,
     // ui: false
-  });
-};
+  })
+}
 
 const html = () => {
-  const courses = JSON.parse(fs.readFileSync("./src/content/courses.json", {encoding: "utf8"}))
-  const team = JSON.parse(fs.readFileSync("./src/content/team.json", {encoding: "utf8"}))
-  const blog = JSON.parse(fs.readFileSync("./src/content/blog.json", {encoding: "utf8"}))
-  const events = JSON.parse(fs.readFileSync("./src/content/events.json", {encoding: "utf8"}))
+  const courses = JSON.parse(
+    fs.readFileSync('./src/content/courses.json', { encoding: 'utf8' })
+  )
+  const team = JSON.parse(
+    fs.readFileSync('./src/content/team.json', { encoding: 'utf8' })
+  )
+  const blog = JSON.parse(
+    fs.readFileSync('./src/content/blog.json', { encoding: 'utf8' })
+  )
+  const events = JSON.parse(
+    fs.readFileSync('./src/content/events.json', { encoding: 'utf8' })
+  )
 
   return gulp
-    .src("./src/pug/page/*.pug")
+    .src('./src/pug/page/*.pug')
     .pipe(
       pug({
         pretty: true,
@@ -37,57 +45,57 @@ const html = () => {
           courses,
           team,
           blog,
-          events
+          events,
         },
       })
     )
-    .pipe(gulp.dest("./dist"))
-    .pipe(browsersync.stream());
-};
+    .pipe(gulp.dest('./dist'))
+    .pipe(browsersync.stream())
+}
 
 const style = () => {
   return gulp
-    .src("./src/scss/main.scss")
+    .src('./src/scss/main.scss')
     .pipe(sass())
-    .pipe(gulp.dest("./dist"))
-    .pipe(browsersync.stream());
-};
+    .pipe(gulp.dest('./dist'))
+    .pipe(browsersync.stream())
+}
 
 const script = () => {
   return gulp
-    .src("./src/js/main.js")
+    .src('./src/js/main.js')
     .pipe(
       webpack({
-        mode: "development",
+        mode: 'development',
         output: {
-          filename: "main.min.js",
+          filename: 'main.min.js',
         },
       })
     )
-    .pipe(gulp.dest("./dist/"))
-    .pipe(browsersync.stream());
-};
+    .pipe(gulp.dest('./dist/'))
+    .pipe(browsersync.stream())
+}
 
 const image = () => {
-  return gulp.src("./src/img/**/*").pipe(gulp.dest("./dist/img"));
-};
+  return gulp.src('./src/img/**/*').pipe(gulp.dest('./dist/img'))
+}
 
 const font = () => {
-  return gulp.src("./src/fonts/**/*").pipe(gulp.dest("./dist/fonts/"));
-};
+  return gulp.src('./src/fonts/**/*').pipe(gulp.dest('./dist/fonts/'))
+}
 
-export { html, style, script, image, font, browserSync };
+export { html, style, script, image, font, browserSync }
 
 export default () => {
-  html();
-  style();
-  script();
-  image();
-  font();
-  browserSync();
-  gulp.watch("./src/pug/**/*.pug", html);
-  gulp.watch("./src/scss/**/*.scss", style);
-  gulp.watch("./src/js/**/*.js", script);
-  gulp.watch("./src/img/**/*", image);
-  gulp.watch("./src/fonts/**/*", font);
-};
+  html()
+  style()
+  script()
+  image()
+  font()
+  browserSync()
+  gulp.watch('./src/pug/**/*.pug', html)
+  gulp.watch('./src/scss/**/*.scss', style)
+  gulp.watch('./src/js/**/*.js', script)
+  gulp.watch('./src/img/**/*', image)
+  gulp.watch('./src/fonts/**/*', font)
+}
